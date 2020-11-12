3c61b24b5977ef7e0c1b39919ecb8d9acd3bf32c
Nomadfool <nomad.fool@gmail.com>
2020-11-12 13:48:07 +0300

    final

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState( 0 );
-
-  function incrementCount() {
-    setCount(prevCount => prevCount  + 1)
-  }
-
-  function decrementCount() {
-    setCount(prevCount => prevCount  - 1)
-  }
   return (
    <div className='App'>
-    <button className='Button' onClick={decrementCount}>-</button>
-    <span className='Span'>{count}</span>
-    <button className='Button' onClick={incrementCount}>+</button>
+    <button className='Button' >-</button>
+    <span className='Span'>0</span>
+    <button className='Button' >+</button>
    </div>
   );
 }
